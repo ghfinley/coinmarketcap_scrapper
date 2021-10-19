ad4be60fac1a4a0fb804ccbc38741a777894279d
Giff Finley <gifffinley@MacBook.attlocal.net>
2021-10-19 08:58:57 -0400

    Deep Link

---
+++ coinmarketcap_deeplink.py
@@ -0,0 +1,14 @@
+import urllib.request #imports the command library
+import os #import the operating system 
+import time #import time 
+import pamdas
+
+if not os.path.exists("deep_link_html")
+	os.mkdir("deep_link_html")
+
+
+ df = pandas.read_csv("parsed_files/coinmarketcap_dataet.csv")  #use pandas to read the csv parsed data file and name to variable df
+
+ print(df)
+ 
+
